faf97279e3131e59cd37c6e4a0b8ea91c180cddf
Andrew Olson <ajolson7@gmail.com>
2018-10-11 17:52:39 -0600

    Added some semicolons in App.js

--- src/App.js
+++ src/App.js
@@ -39,16 +39,16 @@ class App extends Component {
   newSession = () => {
     //get random code either from firebase that doesn't yet exist, or generate new ones until an unused one is found
     this.setState({room_code: 'AB5X3'});
-  }
+  };
 
   joinSession = (code) => {
     //TODO check code to see if room exists first
     this.setState({room_code: code});
-  }
+  };
 
   clearSession = () => {
     this.setState({room_code: ""});
-  }
+  };
 
   render() {
     return (
